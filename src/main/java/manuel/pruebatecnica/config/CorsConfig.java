2660b3de97919824ba211eec03670b63422fc46a
ManuelNinyo <mfnr_93@hotmail.com>
2025-12-11 13:42:27 -0500

    fix cors

--- src/main/java/manuel/pruebatecnica/config/CorsConfig.java
+++ src/main/java/manuel/pruebatecnica/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("https://manuelninyo.github.io/PruebaTecnicaLT/");
+        config.addAllowedOrigin("https://manuelninyo.github.io/PruebaTecnicaLT");
         config.addAllowedHeader("Authorization");
         config.addAllowedHeader("Content-Type");
         config.addAllowedHeader("Accept");
